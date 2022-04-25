7207bd47a92dd5cde467af6ec7a2af20489a97b7
Ruslan <miraxle@yandex.ru>
2022-04-25 10:21:53 +0300

    Добавлен Spring Repository для Product, реализован метод поиска

--- src/main/java/ru/saubulprojects/searchapp/repository/ProductRepository.java
+++ src/main/java/ru/saubulprojects/searchapp/repository/ProductRepository.java
@@ -1,6 +1,9 @@
 package ru.saubulprojects.searchapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ru.saubulprojects.searchapp.model.Product;
@@ -8,4 +11,10 @@ import ru.saubulprojects.searchapp.model.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	
+	@Query("SELECT p FROM Product p "
+		 + "WHERE p.name LIKE CONCAT('%', :query, '%')"
+		 + "OR p.description LIKE CONCAT('%', :query, '%'")
+	public List<Product> searchProducts(String query);
+	
 }
+
